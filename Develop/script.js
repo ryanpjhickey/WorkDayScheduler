35319f795985184352f326a4e0d0820cf7078bd0
Ryan <ryanpjhickey@gmail.com>
2022-08-08 10:40:47 -0700

    broken moment js code; .add is not a function

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,35 @@
-var workHours = ["8:00", "9:00", "10:00"];
+var work8 = document.querySelector("#work8");
+var work9 = document.querySelector("#work9");
+var work10 = document.querySelector("#work10");
+var work11 = document.querySelector("#work11");
+var work12 = document.querySelector("#work12");
+var work1 = document.querySelector("#work1");
+var work2 = document.querySelector("#work2");
+var work3 = document.querySelector("#work3");
+var work4 = document.querySelector("#work4");
+
+var workHours = [
+  work8,
+  work9,
+  work10,
+  work11,
+  work12,
+  work1,
+  work2,
+  work3,
+  work4,
+];
 
 for (let index = 0; index < workHours.length; index++) {
-  console.log(workHours[index]);
+  workStart;
 }
 
-var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
-console.log(rightNow);
+// momentjs workStart
 
-var tomorrow = moment().add(1, "day").format("dddd, MM-D-YYYY [at] hh:mm:ss A");
-console.log(tomorrow);
+var workStart = moment("8:00", "HH:mm").format("HH:mm");
+console.log(workStart);
 
-var pastDate = moment("12-01-1999", "MM-DD-YYYY").format("dddd, MM/DD/YY");
-console.log(pastDate);
+workHours[0].textContent = workStart;
+workHours[1].textContent = workStart.add(1, "hours").format("HH:mm");
+
+//end momentjs

daad9a3f9dc7d0b6993a3b2705f7a03aca26279c
Ryan <ryanpjhickey@gmail.com>
2022-08-08 10:07:28 -0700

    test momentjs library

--- Develop/script.js
+++ Develop/script.js
@@ -4,4 +4,11 @@ for (let index = 0; index < workHours.length; index++) {
   console.log(workHours[index]);
 }
 
-console.log();
+var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+console.log(rightNow);
+
+var tomorrow = moment().add(1, "day").format("dddd, MM-D-YYYY [at] hh:mm:ss A");
+console.log(tomorrow);
+
+var pastDate = moment("12-01-1999", "MM-DD-YYYY").format("dddd, MM/DD/YY");
+console.log(pastDate);
